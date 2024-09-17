1722a0f3c32d85cf7b8135626b0b7f7e6ea29dab
Robert DeRieux <ppldiecomplex@gmail.com>
2024-09-17 15:36:12 -0600

    Add a function to push all even numbers to evens, and all odd numbers to odds

--- index.js
+++ index.js
@@ -10,17 +10,31 @@ const numbers = {
   evens: [],
 };
 
-//create a function that moves a single number
+// Create a function that moves a single number to the bank
 function addNumberToBank(number) {
   numbers.bank.push(number);
 }
 
+// Create a function that moves the first number of the bank
+// and pushes it to evens if even, odds if odd
 function firstNumberToOddOrEven() {
   const number = numbers.bank.shift();
   if (number % 2 === 0) {
     numbers.evens.push(number);
   } else {
     numbers.odds.push(number);
+  }
+}
+
+// Create function that loops through all numbers in the bank
+function allNumbersToOddOrEven(numbers) {
+  for (let i = 0; i < numbers.length; i++) {
+    const number = numbers.bank[i];
+    if (number % 2 === 0) {
+      numbers.evens.push(number);
+    } else {
+      numbers.odds.push(number);
+    }
   }
 }
 
@@ -49,9 +63,22 @@ function renderOddNumbers() {
   $output.replaceChildren(...$numbers);
 }
 
+// render the even numbers to the evens output
+function renderEvenNumbers() {
+  const $numbers = numbers.evens.map((number) => {
+    const $number = document.createElement("span");
+    $number.textContent = number;
+
+    return $number;
+  });
+  const $output = document.querySelector("#evens output");
+  $output.replaceChildren(...$numbers);
+}
+
 function render() {
   renderNumbersToBank();
   renderOddNumbers();
+  renderEvenNumbers();
 }
 
 //  === Script ===
@@ -78,3 +105,5 @@ $section.addEventListener("click", (event) => {
 
   render();
 });
+
+// add event listener for sort all button
