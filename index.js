a7d1b6d2796e676d4d86755cf6122b1c91414717
Robert DeRieux <ppldiecomplex@gmail.com>
2024-09-17 12:54:07 -0600

    Add the numbers object with three arrays

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // TODO: this file! :)
+// create an object with three arrays
+// one to store the number bank numbers
+// one for odd numbers
+// one for even numbers
+const numbers = {
+  bank: [],
+  odds: [],
+  evens: [],
+};
